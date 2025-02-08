676f823b800f1782d97bde987230c5ea4678f575
Ajoy Sarkar <155616148+Anondoraydev@users.noreply.github.com>
2025-02-08 13:53:09 +0600

    coffee delete method add

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 require('dotenv').config();
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app = express();
 const port = process.env.PORT || 5000;
 
@@ -34,7 +34,7 @@ async function run() {
         app.get('/coffee', async (req, res) => {
 
             const cursor = coffeeCollection.find();  
-            const result = await cursor.toArray();  
+            const result = await cursor.toArray(); 
             res.send(result);
         });
 
@@ -46,6 +46,14 @@ async function run() {
             const result = await coffeeCollection.insertOne(newCoffee);
             res.send(result)
 
+        })
+
+
+        app.delete('/coffee/:id', async (req, res)=>{
+            const id = req.params.id;
+            const query ={_id: new ObjectId(id)}
+            const result =await coffeeCollection.deleteOne(query)
+            res.send(result)
         })
 
 
